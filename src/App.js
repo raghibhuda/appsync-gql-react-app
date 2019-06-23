7595af0eeac634e760b72cae663228ba15e009c9
Ragib Ahsanul <raghibhuda@gmail.com>
2019-06-23 15:27:46 +0600

    read and create note features added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { API, graphqlOperation } from 'aws-amplify';
 
@@ -12,37 +11,86 @@ const allNotes = `
     }
   }`
 
-
+const createNoteMutation = `
+  mutation ($name:String! $description:String ){
+    createNotes(input:{
+      name:$name
+      description: $description
+    }){
+      id name description
+    }
+  }
+`
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      notes: []
+      notes: [],
+      name: '',
+      description: ''
     }
   }
-  async componentDidMount (){
-    let notes = await API.graphql(graphqlOperation(allNotes));
-    console.log('===========Notes==========',notes);
+  async componentDidMount() {
+    const data = await API.graphql(graphqlOperation(allNotes));
+    console.log('===========Notes==========', data);
+    this.setState({
+      notes: data.data.listNotess.items
+    })
+    console.log(this.state.notes)
   }
+
+  handeNameChange = (e) => {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+
+  handeDescriptionChange = (e) => {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  createNote = async (e) => {
+    e.preventDefault();
+    const newNote  =  {
+      name: this.state.name,
+      description:this.state.description
+    }
+
+    try{
+      const notes = [...this.state.notes,newNote];
+      this.setState({
+        notes,
+        name:'',
+        description:''
+      })
+      await API.graphql(graphqlOperation(createNoteMutation,newNote));
+      console.log('Successfully added ');
+    }catch(error){
+      console.log(error)
+    }
+
+  }
+
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
+        <div style={{ padding: 25, display: 'flex', flexDirection: 'column' }}>
+          <input type="text" name="name" placeholder="Task Name" onChange={this.handeNameChange} />
+          <textarea style={{ paddingTop: 40, marginTop: 30 }} type="text" name="description" placeholder="Task Description" onChange={this.handeDescriptionChange}></textarea>
+          <button type="submit" onClick={this.createNote} >Add Task</button>
+        </div>
+
+        {
+          this.state.notes.map((c, i) => (<div key={i}>
+            <h2>{c.name}</h2>
+            <p>{c.description}</p>
+          </div>))
+        }
       </div>
     );
   }
